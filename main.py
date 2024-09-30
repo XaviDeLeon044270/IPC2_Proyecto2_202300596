181366ad979599712788b4eacd2e14974f1479a7
Chaval044270 <3033373110108@ingenieria.usac.edu.gt>
2024-09-29 23:27:02 -0600

    cuarto commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+from xml.etree import ElementTree
+from werkzeug.utils import secure_filename
 
 class MyApp:
     def __init__(self):
@@ -7,7 +10,7 @@ class MyApp:
 
     def setup_routes(self):
         self.app.add_url_rule('/', 'home', self.home)
-        self.app.add_url_rule('/archivo', 'archivo', self.archivo)
+        self.app.add_url_rule('/archivo', 'archivo', self.archivo, methods=['GET', 'POST'])
         self.app.add_url_rule('/info', 'info', self.info)
         self.app.add_url_rule('/reportes', 'reportes', self.reportes)
 
@@ -15,7 +18,16 @@ class MyApp:
         return render_template('index.html')
 
     def archivo(self):
-        return render_template('archivo.html')
+        if request.method == 'POST':
+            file = request.files.get('fileInput')
+            if file and file.filename.endswith('.xml'):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join('uploads', filename))
+                return 'Archivo subido con éxito'
+            else:
+                return 'No se seleccionó ningún archivo o el archivo no es válido'
+        else:
+            return render_template('archivo.html')
 
     def info(self):
         return render_template('info.html')
